e59a630a8b91c0e045870e7b112c0e5915e456f0
wusheng233 <wusheng233@qq.com>
2024-09-15 19:53:32 +0800

    1-1

---
+++ chapter/1.1.php
@@ -0,0 +1,14 @@
+<?php
+// Chapter 1-1 by minqwq
+// wusheng233移植
+echo "幻想乡，某座山上 | 2017-04-08\n";
+fgets(STDIN);
+echo "<???> 抱歉----麻烦能打扰一下吗？\n";
+fgets(STDIN);
+echo "我听到一个可爱的声音叫住了我。\n";
+fgets(STDIN);
+echo "<???> 厕...厕所在哪里？\n";
+fgets(STDIN);
+echo "<minqwq> 这条路前面有个分岔路口，你左转应该就能看到了。\n";
+fgets(STDIN);
+echo "<???> (快速离开)\n";
