984bc878a239555466f866029c2ed184fbc5ef0e
wusheng233 <wusheng233@qq.com>
2024-09-21 14:21:12 +0800

    更新一大坨

--- chapter/1.2.php
+++ chapter/1.2.php
@@ -1,13 +1,27 @@
 <?php
 // This chapter by MeltedIce and mixed by minqwq
 // wusheng233移植
-echo "没想好用什么音乐。\n";
+echo "音乐:Bored House | by minqwq\n";
 echo "幻想乡，山对面树林里某间屋子 | 2017-XX-XX\n";
 fgets(STDIN);
 echo "唔~起床了\n";
 fgets(STDIN);
 echo "今天是去新学校报道的第一天，我可不能迟到。\n";
 fgets(STDIN);
+echo "(出门前的神秘准备工作)\n";
+fgets(STDIN);
+echo "(离开)\n";
+echo "\n幻想乡，727号公路 | 2017-04-09\n";
+echo "少女走路中...(按下 Enter 键以前进。)";
+for($i = 1;$i <= 20;$i++) {
+	fgets(STDIN);
+	echo $i / 20 * 100 . "%";
+}
+echo "\n为防止剧本展示过快，请缓慢的再按 3 次 Enter 键以继续。\n";
+for($i = 0;$i < 3;$i++) {
+	fgets(STDIN);
+}
+echo "音乐:Road 727 | by minqwq\n";
 echo "来到教室，这里熙熙攘攘的，坐满了新同学\n";
 fgets(STDIN);
 echo "我用着视线瞟着新同学们\n";
@@ -40,4 +54,96 @@ echo "她的脸染上了红晕，扭捏着\n";
 fgets(STDIN);
 echo "<minqwq> 很高兴认识你！我的名字是minqwq，平常直接叫我min就行。\n";
 fgets(STDIN);
-echo "\n";
+echo "随后，上课铃响起了。\n";
+fgets(STDIN);
+echo "我们俩随后立即摆正坐姿\n";
+fgets(STDIN);
+echo "少女上课中...\n";
+for($i = 1;$i <= 45;$i++) {
+	usleep(250000);
+	echo round($i / 45 * 100) . "%\n";
+}
+echo "音乐:Night of code | by minqwq\n";
+echo "转眼见外面的太阳都快要落下去了。\n";
+fgets(STDIN);
+echo "<minqwq> 再见\n";
+fgets(STDIN);
+echo "<Yukari> 再见~！\n";
+fgets(STDIN);
+echo "我们俩就这么告了别，随后我打算去咖啡厅喝杯咖啡\n";
+fgets(STDIN);
+echo "(出门)\n";
+fgets(STDIN);
+echo "<minqwq> 虽然走夜路很害怕，但是还是想要去喝一杯咖啡再回家啊...\n";
+fgets(STDIN);
+echo "于是我还是打算去来一杯咖啡。\n";
+fgets(STDIN);
+echo "少女黑夜行走中...\n";
+$r = 101;
+for($i = 1;$i <= 120;$i++) {
+	usleep(100000);
+	$v = round($i / 120 * 100);
+	if($v !== $r) {
+		echo $v . "%\n";
+		$r = $v;
+	}
+}
+echo "<minqwq> 终于到地方了...\n";
+fgets(STDIN);
+echo "(推门)\n";
+fgets(STDIN);
+echo "<minqwq> 老板...?\n";
+fgets(STDIN);
+echo "<Safari> min...?\n";
+fgets(STDIN);
+echo "<minqwq> 你怎么穷的都来打工了(\n";
+fgets(STDIN);
+echo "<Safari> 不是啦，其实就是想整点小钱买台手机而已\n";
+fgets(STDIN);
+echo "<minqwq> 所以\n";
+fgets(STDIN);
+echo "<minqwq> 来一杯多糖咖啡\n";
+fgets(STDIN);
+echo "<Safari> 好\n";
+fgets(STDIN);
+echo "福瑞制咖啡中...\n";
+$r = 101;
+for($i = 1;$i <= 2012;$i++) {
+	usleep(10000);
+	$v = round($i / 2012 * 100);
+	if($v !== $r) {
+		echo $v . "%\n";
+		$r = $v;
+	}
+}
+echo "<Safari> 好了\n";
+fgets(STDIN);
+echo "(拿)\n";
+fgets(STDIN);
+echo "<双方> (微笑\n";
+fgets(STDIN);
+echo "(闻)\n";
+fgets(STDIN);
+echo "<minqwq> 不赖啊\n";
+fgets(STDIN);
+echo "<Safari> 那肯定，上一任老板亲自教我的\n";
+fgets(STDIN);
+echo "(坐在店铺的某个角落里开始饮用)\n";
+echo "少女饮咖中...\n";
+$r = 101;
+for($i = 1;$i <= 2085;$i++) {
+	usleep(10000);
+	$v = round($i / 2085 * 100);
+	if($v !== $r) {
+		echo $v . "%\n";
+		$r = $v;
+	}
+}
+echo "<minqwq> 那我先走了，明天虽然不上学但是有代码没写\n";
+fgets(STDIN);
+echo "<Safari> 好~\n";
+fgets(STDIN);
+echo "现在处于 Another View 状态\n";
+echo "<Safari> 又无聊了...\n";
+fgets(STDIN);
+echo "现在处于 Player View 状态\n";
